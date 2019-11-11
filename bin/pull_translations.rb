f9315106a5f639bff86ed310c285a68ad791e2cf
Gerhard Schlager <mail@gerhard-schlager.at>
2019-11-11 15:19:52 +0100

    Update translations
    Excludes all translations when they are the same in German and German-Formal

--- bin/pull_translations.rb
+++ bin/pull_translations.rb
@@ -8,12 +8,36 @@ gemfile(true) do
 end
 
 require 'translations_manager'
+require "translations_manager/duplicate_remover"
+require 'yaml'
 
-YML_DIRS = ['config/locales'].map { |d| File.expand_path(d) }
+YML_DIRS = ['../../config/locales'].map { |d| File.expand_path(d, __FILE__) }
 YML_FILE_PREFIXES = ['client', 'server']
 TRANSIFEX_LOCALE = 'de_DE'
 DISCOURSE_LOCALE = 'de_formal'
+LOCALES = ['de_DE', 'de']
+
+def remove_duplicates
+  YML_DIRS.each do |dir|
+    YML_FILE_PREFIXES.each do |prefix|
+      source_filename = File.join(dir, "#{prefix}.de.yml")
+      target_filename = File.join(dir, "#{prefix}.de_formal.yml")
+
+      if File.exist?(source_filename)
+        if File.exist?(target_filename)
+          TranslationsManager::DuplicateRemover.new(source_filename, target_filename).clean!
+        end
+
+        File.delete(source_filename)
+      end
+    end
+  end
+end
 
 TranslationsManager::TransifexUpdater
-  .new(YML_DIRS, YML_FILE_PREFIXES, TRANSIFEX_LOCALE)
-  .perform(update_tx_config: false, language_map: { TRANSIFEX_LOCALE => DISCOURSE_LOCALE })
+  .new(YML_DIRS, YML_FILE_PREFIXES, *LOCALES)
+  .after_pull { remove_duplicates }
+  .perform(
+    update_tx_config: false,
+    language_map: { TRANSIFEX_LOCALE => DISCOURSE_LOCALE }
+  )
